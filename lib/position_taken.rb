f0ad21645164212bb88453e95940c73a44604765
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-20 16:11:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken? (board, index)
  if board[index-1] == " " || board[index-1] == "" || board[index-1] == nil
    return false
- elseif board[index-1] == "X" || board[index-1] == "O"
+else board[index-1] == "X" || board[index-1] == "O"
   return true
  end
 end
